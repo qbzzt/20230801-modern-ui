d397fa6fda2d622bc76d177e61519ae8ec7c4d08
Ori Pomerantz <qbzzt1@gmail.com>
2023-11-17 15:28:15 -0600

    WIP

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -4,28 +4,33 @@ import { holesky, sepolia } from 'wagmi/chains'
 import { publicProvider } from 'wagmi/providers/public'
 
 import { Chain } from 'wagmi'
+import { defineChain } from 'viem'
 
 const walletConnectProjectId = 'c96e690bb92b6311e8e9b2a6a22df575'
 
-const redstoneHolesky = {
+const redstoneHolesky = defineChain({
   id: 17_001,
   name: 'Redstone Holesky',
   network: 'redstone-holesky',
   nativeCurrency: {
-    name: 'Holesky Ether',
-    symbol: 'ETH',
     decimals: 18,
+    name: 'Ether',
+    symbol: 'ETH',
   },
   rpcUrls: {
-    default: { http: ['https://rpc.holesky.redstone.xyz'], },
-    public: { http: ['https://rpc.holesky.redstone.xyz'], },
+    default: {
+      http: ['https://rpc.holesky.redstone.xyz'],
+      webSocket: ['wss://rpc.holesky.redstone.xyz/ws'],
+  },
+  public: {
+      http: ['https://rpc.holesky.redstone.xyz'],
+      webSocket: ['wss://rpc.holesky.redstone.xyz/ws'],
+    },
   },
   blockExplorers: {
-    default: { name: 'Redstone Holesky', 
-               url: 'https://explorer.holesky.redstone.xyz' },
+    default: { name: 'Explorer', url: 'https://explorer.holesky.redstone.xyz' },
   },
-}  as const satisfies Chain
-
+})
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [ holesky, sepolia, redstoneHolesky ],
