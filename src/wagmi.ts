21f603ca4c72d7541cb36f453eb52d1e5cf2e112
Ori Pomerantz <qbzzt1@gmail.com>
2023-11-17 10:02:21 -0600

    Maybe the final version

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -3,13 +3,33 @@ import { configureChains, createConfig } from 'wagmi'
 import { holesky, sepolia } from 'wagmi/chains'
 import { publicProvider } from 'wagmi/providers/public'
 
+import { Chain } from 'wagmi'
+
 const walletConnectProjectId = 'c96e690bb92b6311e8e9b2a6a22df575'
 
+const redstoneHolesky = {
+  id: 17_001,
+  name: 'Redstone Holesky',
+  network: 'redstone-holesky',
+  nativeCurrency: {
+    name: 'Holesky Ether',
+    symbol: 'ETH',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: { http: ['https://rpc.holesky.redstone.xyz'], },
+    public: { http: ['https://rpc.holesky.redstone.xyz'], },
+  },
+  blockExplorers: {
+    default: { name: 'Redstone Holesky', 
+               url: 'https://explorer.holesky.redstone.xyz' },
+  },
+}  as const satisfies Chain
+
+
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [ holesky, sepolia ],
-  [
-    publicProvider(),
-  ],
+  [ holesky, sepolia, redstoneHolesky ],
+  [ publicProvider(), ],
 )
 
 const { connectors } = getDefaultWallets({
@@ -26,3 +46,4 @@ export const config = createConfig({
 })
 
 export { chains }
+
